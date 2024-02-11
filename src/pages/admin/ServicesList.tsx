142667d671af38d4ccd4281b898fd2e594c234bc
hasankarim18 <hallysust@gmail.com>
2024-02-11 13:50:33 +0600

    query using tanstack query

--- src/pages/admin/ServicesList.tsx
+++ src/pages/admin/ServicesList.tsx
@@ -1,7 +1,22 @@
+import { getServices } from "@/adminApi/services/service.api";
+import { useQuery } from "@tanstack/react-query";
+
 const ServicesList = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["services"],
+    queryFn: getServices,
+  });
+
+  if (isLoading) {
+    return <h1>Loading....</h1>;
+  }
+
   return (
     <div>
       <h1>Services list</h1>
+      <ul className="mt-2">
+        {!isLoading && data.data.map((item, i) => <li key={i}>{item.name}</li>)}
+      </ul>
     </div>
   );
 };
