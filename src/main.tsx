33fd0c08a96860d7d9724cea0a68edf21b31ba35
hasankarim18 <hallysust@gmail.com>
2024-02-12 12:09:15 +0600

    data fetching hook of tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
@@ -8,11 +7,17 @@ import router from "./routes/routes.tsx";
 
 const queryClient = new QueryClient();
 
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <QueryClientProvider client={queryClient}>
+//       <RouterProvider router={router} />
+//       <ReactQueryDevtools initialIsOpen={false} />
+//     </QueryClientProvider>
+//   </React.StrictMode>
+// );
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
--- src/pages/admin/ServicesList.tsx
+++ src/pages/admin/ServicesList.tsx
@@ -1,22 +1,76 @@
-import { getServices } from "@/adminApi/services/service.api";
-import { useQuery } from "@tanstack/react-query";
+import { useGetServices } from "@/adminApi/services/service.hook";
+import Container from "@/components/Container";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Trash2 } from "lucide-react";
 
 const ServicesList = () => {
-  const { data, isLoading } = useQuery({
-    queryKey: ["services"],
-    queryFn: getServices,
-  });
+  const { data: services, isLoading, isError } = useGetServices();
+
+  if (isError) {
+    return (
+      <p className="text-center h-screen flex items-center justify-center font-semibold p-4 bg-rose-400 text-white text-2xl">
+        Some thing went wrong!!!
+      </p>
+    );
+  }
 
   if (isLoading) {
     return <h1>Loading....</h1>;
   }
 
+  console.log(services);
+
   return (
     <div>
-      <h1>Services list</h1>
-      <ul className="mt-2">
-        {!isLoading && data.data.map((item, i) => <li key={i}>{item.name}</li>)}
-      </ul>
+      <Container className="mt-20 border-[1px] p-0 border-gray rounded-xl overflow-hidden">
+        <h2 className="text-3xl p-2">Services list</h2>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Service Name</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead className="text-right">Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {services.map((item) => {
+              return (
+                <TableRow key={item.id}>
+                  <TableCell className="font-medium">{item.name}</TableCell>
+                  <TableCell>{item.description}</TableCell>
+                  <TableCell>{item.price}</TableCell>
+                  <TableCell className="text-right">
+                    <Button
+                      className="bg-red-400 rounded-xl p-2 text-white"
+                      variant="destructive"
+                    >
+                      <Trash2 />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+          <TableFooter className="bg-light-gray">
+            <TableRow>
+              <TableHead className="w-[100px]">Total Services</TableHead>
+              <TableHead></TableHead>
+              <TableHead></TableHead>
+              <TableHead className="text-right">{services.length}</TableHead>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </Container>
     </div>
   );
 };
